c80b5d8b7e35de24a584e11f7f75ec3a4312fd3a
Amberside <47558882+Amberside@users.noreply.github.com>
2020-08-05 09:44:31 +1000

    Header.js
    Added code to Header.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 
+// Import custom components
+import Header from './components/layout/Header';
+
 class App extends Component {
   render(){
     return (
       <div className="App">
         <h1>Contact Manager</h1>
+        <Header />
       </div>
     );
   }
---
+++ src/components/layout/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+const Header = props => {
+  return (
+    <div>
+      <p>This is the header</p>
+    </div>
+  )
+}
+
+Header.propTypes = {
+  
+}
+
+export default Header
