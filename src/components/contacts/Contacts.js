43a0d1df7818470add470313beb661ac3d3ae750
Amberside <47558882+Amberside@users.noreply.github.com>
2020-08-05 10:29:35 +1000

    Update Contacts.js
    moved export to bottom of the file.

--- src/components/contacts/Contacts.js
+++ src/components/contacts/Contacts.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
-export default class Contacts extends Component {
+class Contacts extends Component {
   render() {
     return (
       <div>
@@ -9,3 +9,5 @@ export default class Contacts extends Component {
     )
   }
 }
+
+export default Contacts
