ad44e38530779c1e737dbac0d3c4d8c5e331b7ec
Amberside <47558882+Amberside@users.noreply.github.com>
2020-08-05 12:05:16 +1000

    Update Contacts.js
    Added map function in Contacts.js

--- src/components/contacts/Contacts.js
+++ src/components/contacts/Contacts.js
@@ -4,15 +4,34 @@ import Contact from './Contact';
 
 class Contacts extends Component {
   render() {
-    const contact = {
-      id: 1,
-      name: 'Jade Doe',
-      email: 'jadedoe@gmail.com',
-      phone: '(555)-444-5555'
-    }
+    const contacts = [
+      {
+        id: 1,
+        name: 'Jade Doe',
+        email: 'jadedoe@gmail.com',
+        phone: '(555)-444-5555'
+      },
+      {
+        id: 2,
+        name: 'Jack Blogs',
+        email: 'jackblogs@gmail.com',
+        phone: '(465)-345-3523'
+      },
+      {
+        id: 3,
+        name: 'Alex Harry',
+        email: 'alexharry@gmail.com',
+        phone: '(723)-942-3842'
+      }
+  ]
     return (
       <div>
-        <Contact contact={contact}/>
+        <h1 className='display-4 text-primary'>Contact List</h1>
+        {
+          contacts.map(contact => (
+            <Contact key={contact.id} contact={contact} />
+          ))
+        }
       </div>
     )
   }
