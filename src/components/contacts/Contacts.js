d61567a64900f68c920e4e1ea73802271215cde9
Amberside <47558882+Amberside@users.noreply.github.com>
2020-08-12 14:48:11 +1000

    Update Contacts.js
    Fixed the state in Contacts.js

--- src/components/contacts/Contacts.js
+++ src/components/contacts/Contacts.js
@@ -5,30 +5,30 @@ import Contact from './Contact';
 class Contacts extends Component {
   state = {
     contacts: [{
-    id: 1,
-    name: 'Jade Doe',
-    email: 'jadedoe@gmail.com',
-    phone: '(555)-444-5555'
-  },
-  {
-    id: 2,
-    name: 'Jack Blogs',
-    email: 'jackblogs@gmail.com',
-    phone: '(465)-345-3523'
-  },
-  {
-    id: 3,
-    name: 'Alex Harry',
-    email: 'alexharry@gmail.com',
-    phone: '(723)-942-3842'
-  }]
-}
+      id: 1,
+      name: 'Jade Doe',
+      email: 'jadedoe@gmail.com',
+      phone: '(555)-444-5555'
+    },
+    {
+      id: 2,
+      name: 'Jack Blogs',
+      email: 'jackblogs@gmail.com',
+      phone: '(465)-345-3523'
+    },
+    {
+      id: 3,
+      name: 'Alex Harry',
+      email: 'alexharry@gmail.com',
+      phone: '(723)-942-3842'
+    }]
+  }
   deleteContact (id) {
     console.log("Deleting contact: " + id);
   }
   
   render() {
-    const {contacts} = this.state;
+    const { contacts } = this.state;
     return (
       <div>
         <h1 className='display-4 text-primary'>Contact List</h1>
