c213230edc3d265c83b9e42fc3eb4e525ead17d6
Amberside <47558882+Amberside@users.noreply.github.com>
2020-08-19 12:11:46 +1000

    Update SingleContact.js
    Add consumer to singleContact.

--- src/components/contacts/SingleContact.js
+++ src/components/contacts/SingleContact.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Consumer } from '../../context';
 
 class SingleContact extends Component {
   state = {
@@ -15,37 +16,45 @@ class SingleContact extends Component {
   delClick = (id) => {
     console.log("delete: " + id);
     // This calls the function in contacts.js (parent component) and will pass the id through.
-    this.props.delContact(id);
+    // this.props.delContact(id);
   }
   
   // This will render our contact object passed from contacts.
   render() {
     const { name, email, phone, id } = this.props.contact;
+    const { showContactInfo } = this.state;
     return (
-      <div className='card card-body mb-3'>
-        <h2>
-          { name }
-          <FontAwesomeIcon icon='sort-down' onClick={this.onShowClick} style={{ cursor: "pointer"}}>
-          </FontAwesomeIcon>
-          {/* This onclick call the local delClick function */}
-          <FontAwesomeIcon icon='times' style={{ cursor: "pointer", float: "right", color: "red"  }}
-            onClick={this.delClick.bind(this, id)}
-          
-          >
-          </FontAwesomeIcon>
-          {' '}
-          <FontAwesomeIcon icon='pencil-alt' style={{ cursor: "pointer", float: "right"  }}>
-          </FontAwesomeIcon>
-        </h2>
-        { this.state.showContactInfo ? (
-          <ul className="list-group">
-            <li className="list-group-item">Email: {email}</li>
-            <li className='list-group-item'>Phone: {phone}</li>
-          </ul>
-        ) : null 
-        }
-      </div>
-    )
+      <Consumer>
+        { value => {
+          const { dispatch } = value; // this is pulling our dispatch function in our Consumer
+          return (
+            <div className='card card-body mb-3'>
+              <h2>
+                { name }
+                <FontAwesomeIcon icon='sort-down' onClick={this.onShowClick} style={{ cursor: "pointer"}}>
+                </FontAwesomeIcon>
+                {/* This onclick call the local delClick function */}
+                <FontAwesomeIcon icon='times' style={{ cursor: "pointer", float: "right", color: "red"  }}
+                  onClick={this.delClick.bind(this, id)}
+                
+                >
+                </FontAwesomeIcon>
+                {' '}
+                <FontAwesomeIcon icon='pencil-alt' style={{ cursor: "pointer", float: "right"  }}>
+                </FontAwesomeIcon>
+              </h2>
+              { showContactInfo ? (
+                <ul className="list-group">
+                  <li className="list-group-item">Email: {email}</li>
+                  <li className='list-group-item'>Phone: {phone}</li>
+                </ul>
+              ) : null 
+              }
+            </div>
+          )}} 
+          {/* this is end the inner return and value sections */}
+      </Consumer>
+    ) // this closes the outer return
   }
 }
 
