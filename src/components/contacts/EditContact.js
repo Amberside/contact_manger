8389358baddfd0c85b3d57e99aeabf4a17d8674b
Amberside <47558882+Amberside@users.noreply.github.com>
2020-08-26 11:54:20 +1000

    Update EditContact.js
    Added API to edit contact

--- src/components/contacts/EditContact.js
+++ src/components/contacts/EditContact.js
@@ -1,24 +1,44 @@
 import React, { Component, Fragment } from 'react';
 import classnames from 'classnames';
 import { Consumer } from '../../context';
+import axios from 'axios';
 
 class EditContact extends Component {
   // The state only holds the errors as we are using refs to hold the values from the form
   state = {
+    name: '',
+    email: '',
+    phone: '',
     errors: {},
   };
   
-  // create the refs to hold the values from the form.
-  nameInput = React.createRef();
-  emailInput = React.createRef();
-  phoneInput = React.createRef();
+  // // create the refs to hold the values from the form.
+  // nameInput = React.createRef();
+  // emailInput = React.createRef();
+  // phoneInput = React.createRef();
 
-  componentDidMount(){
-    // update state in here
+  async componentDidMount(){
+    // get our id from our url.
+    const { id } = this.props.match.params;
+    // did a console log { id: "5"} depending one which contact we clicked one.
+    
+    const res = await axios.get(`https://jsonplaceholder.typicode.com/users/${id}`);
+    const contact = res.data;
+    this.setState({
+      name: contact.name,
+      email: contact.email,
+      phone: contact.phone
+    });
+  }
+  
+  onChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
   }
   
   // this function will be called when the form is submitted.
-  handleSubmit = (dispatch, e) => {
+  handleSubmit = async(dispatch, e) => {
     e.preventDefault();
     console.log("Submit: update")
 
@@ -26,10 +46,13 @@ class EditContact extends Component {
     // Pull the id out of the url
     const { id } = this.props.match.params;
     // get the current value from the form using the refs we set up.
-    const name = this.nameInput.current.value;
-    const email = this.emailInput.current.value;
-    const phone = this.phoneInput.current.value;
-    console.log(name + ' ' + email + ' ' + phone)
+    // const name = this.nameInput.current.value;
+    // const email = this.emailInput.current.value;
+    // const phone = this.phoneInput.current.value;
+    // console.log(name + ' ' + email + ' ' + phone)
+    const { name, email, phone } = this.state;
+    // shortcut so we don't have to type:
+    // this.state.name or this.state.email or this.state.phone
     // Check for Errors
     
     if (name === ''){
@@ -52,7 +75,6 @@ class EditContact extends Component {
     
     // create a updated Contact object 
     const updContact = {
-      id,
       name,
       email,
       phone
@@ -60,27 +82,29 @@ class EditContact extends Component {
     
      // send the updated contact to an api or state managment.
     console.log(updContact);
+    
+    const res = await axios.put(`https://jsonplaceholder.typicode.com/users/${id}`, updContact);
+    
     // this is where we would call our dispatch function
     // dispatch our updated contact to the global state
-    dispatch({ type: 'UPDATE_CONTACT', payload: updContact});
+    dispatch({ type: 'UPDATE_CONTACT', payload: res.data});
     // redirect the browser back to the contacts page ('/')
     this.props.history.push("/");
   }
   
   render() {
     // pull out the id from the url
-    const cid = this.props.match.params;
+    // const cid = this.props.match.params;
     // { id: "1" } (depending on the contact you are editing. )
     // pull the errors out of state.
-    const { errors } = this.state;
+    const { name, email, phone, errors } = this.state;
     return (
       <Consumer>
         { value => {
-          const { dispatch, contacts } = value;
+          const { dispatch } = value;
           // get the contact from the state by finding the contact in the 
           // contacts array by matching the cont.id to the cid from the url
-          const contact = contacts.find(cont => cont.id === cid.id);
-          
+          // const contact = contacts.find(cont => cont.id === cid.id);
           return (
             <Fragment>
               <h1 className="display-4 text-primary">Edit Contact</h1>
@@ -96,9 +120,10 @@ class EditContact extends Component {
                         placeholder="Name"
                         name="name"
                         // set the default value for the form (contact.name)
-                        defaultValue={contact.name}
+                        // defaultValue={contact.name}
                         // use the nameInput ref here
-                        ref={this.nameInput}
+                        value={name}
+                        onChange={this.onChange}
                         
                       />
                       {errors.name && <div className='invalid-feedback'>{errors.name}</div>}
@@ -111,9 +136,9 @@ class EditContact extends Component {
                         placeholder="Email"
                         name="email"
                         // set the default value for the form (contact.email)
-                        defaultValue={contact.email}
+                        value={email}
                         // use the emailInput ref here
-                        ref={this.emailInput}
+                        onChange={this.onChange}
                         
                       />
                       {errors.email && <div className='invalid-feedback'>{errors.email}</div>}
@@ -126,9 +151,9 @@ class EditContact extends Component {
                         placeholder="Phone"
                         name="phone"
                         // set the default value for the form (contact.phone)
-                        defaultValue={contact.phone}
+                        value={phone}
                         // use the phoneInput ref here
-                        ref={this.phoneInput}
+                        onChange={this.onChange}
                       />
                       {errors.phone && <div className='invalid-feedback'>{errors.phone}</div>}
                     </div>
